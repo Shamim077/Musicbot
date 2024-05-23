0904a0d43180280cf5dd6dbab117b0bd7077922d
Shamim077 <168723582+Shamim077@users.noreply.github.com>
2024-05-23 16:52:19 +0530

    Update stream.py

--- AnonXMusic/utils/stream/stream.py
+++ AnonXMusic/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from AnonXMusic import Carbon, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import add_active_video_chat, is_active_chat
-from AnonXMusic.utils.exceptions import AssistantErr
-from AnonXMusic.utils.inline import aq_markup, close_markup, stream_markup
-from AnonXMusic.utils.pastebin import AnonyBin
-from AnonXMusic.utils.stream.queue import put_queue, put_queue_index
-from AnonXMusic.utils.thumbnails import get_thumb
+from Musicbot import Carbon, YouTube, app
+from Musicbot.core.call import Anony
+from Musicbot.misc import db
+from Musicbot.utils.database import add_active_video_chat, is_active_chat
+from Musicbot.utils.exceptions import AssistantErr
+from Musicbot.utils.inline import aq_markup, close_markup, stream_markup
+from Musicbot.utils.pastebin import AnonyBin
+from Musicbot.utils.stream.queue import put_queue, put_queue_index
+from Musicbot.utils.thumbnails import get_thumb
 
 
 async def stream(
