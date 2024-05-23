20aadd352d144573956ec26737ecd4e70ea8377f
Shamim077 <168723582+Shamim077@users.noreply.github.com>
2024-05-23 16:50:33 +0530

    Update queue.py

--- AnonXMusic/utils/stream/queue.py
+++ AnonXMusic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from AnonXMusic.misc import db
-from AnonXMusic.utils.formatters import check_duration, seconds_to_min
+from Musicbot.misc import db
+from Musicbot.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
