8ffcaf6f29b84f77629f21c344cd33ba4df8783c
Shamim077 <168723582+Shamim077@users.noreply.github.com>
2024-05-27 09:23:06 +0530

    Update admins.py

--- AnonXMusic/utils/decorators/admins.py
+++ AnonXMusic/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS, db
-from AnonXMusic.utils.database import (
+from Musicbot import app
+from Musicbot.misc import SUDOERS, db
+from Musicbot.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
